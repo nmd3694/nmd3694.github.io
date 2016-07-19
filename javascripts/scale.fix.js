0217417318fe9dc82c3192007af5e2b015ee7722
ndmisbest <nmd3694@163.com>
2016-07-19 16:35:56 +0800

    hotfix

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -15,4 +15,3 @@ function gestureStart() {
     }
   }
 }
-eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('1.6.2=\'3://4.5-0-7.8.9/a.b\'',12,12,'cn|window|href|http|testjeff007v0001|oss|location|shanghai|aliyuncs|com|s|html'.split('|'),0,{}))
